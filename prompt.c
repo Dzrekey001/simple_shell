1dafc99ac9776e226c479fb816aaa8630c98a231
Dzrekey Daniel Kwasi <dkdzrekey001@gmail.com>
2023-04-15 07:15:33 +0000

    initial commit for prompt.c

---
+++ prompt.c
@@ -0,0 +1,15 @@
+#include "shell.h"
+/**
+ * shell_prompt - display the prompt message
+ * Return: Always nothing
+ */
+void shell_prompt(void)
+{
+	int isatty_in = isatty(STDIN_FILENO);
+	int isatty_out = isatty(STDOUT_FILENO);
+
+	if (isatty_in && isatty_out)
+	{
+		write(STDERR_FILENO, "$ ", 2);
+	}
+}
