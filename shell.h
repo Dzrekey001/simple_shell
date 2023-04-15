82936f08123e573092f92090c982f4614b2dac5a
Dzrekey Daniel Kwasi <dkdzrekey001@gmail.com>
2023-04-15 07:10:46 +0000

    initial commit for shell.h

---
+++ shell.h
@@ -0,0 +1,26 @@
+#ifndef _SHELL_H_
+#define _SHELL_H_
+#define MAX_LINE_LENGHT 64
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <readline/readline.h>
+#include <readline/history.h>
+
+extern char **environ;
+
+void shell_prompt(void);
+void free_me(char **ptr);
+char *check_env_path();
+char **read_line();
+char **get_arguments(char *str);
+void execute_command(char *program_path, char **program_arg);
+
+/*Utils*/
+char *concatenate_format(char *str1, char *str2);
+
+#endif
