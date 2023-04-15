0bffbebe3b8efdeaf9e61a4fb525df2869c8551c
Dzrekey Daniel Kwasi <dkdzrekey001@gmail.com>
2023-04-15 07:26:33 +0000

    initial commit for read_lines.c

---
+++ read_lines.c
@@ -0,0 +1,39 @@
+#include "shell.h"
+/**
+ * read_line - read input from the stdin
+ * Return: Null if an error occour else return
+ */
+
+char **read_line()
+{
+	char **buffer;
+	size_t str;
+	size_t MAX_LENGHT = MAX_LINE_LENGHT;
+
+	buffer = malloc(sizeof(char *) * MAX_LENGHT);
+	if (buffer == NULL)
+	{
+		free(buffer);
+		return (NULL);
+	}
+	str = getline(buffer, &MAX_LENGHT, stdin);
+	if (str == (size_t) -1)
+	{
+		free(buffer);
+		return (NULL);
+	}
+	while (str > MAX_LENGHT - 1)
+	{
+		MAX_LENGHT *= 2;
+		buffer = realloc(buffer, MAX_LENGHT);
+		if (buffer == NULL)
+			return (NULL);
+		str = getline(buffer, &MAX_LENGHT, stdin);
+		if (str == (size_t) -1)
+		{
+			free(buffer);
+			return (NULL);
+		}
+	}
+	return (buffer);
+}
