36487a783514f4bf947505b972ac12331072aabb
Dzrekey Daniel Kwasi <dkdzrekey001@gmail.com>
2023-04-16 22:26:27 +0000

    Initial commit of built_ins.c

--- built_ins.c
+++ built_ins.c
@@ -1,13 +1,22 @@
 #include "shell.h"
+/**
+ * cd - function to change directory
+ * @program_path: directory path
+ * Return: 0 on success
+ */
 
 int cd(char  *program_path)
 {
-    if (program_path == NULL)
-        program_path = "/";     
-    return (chdir(program_path));
+	if (program_path == NULL)
+		program_path = "/";
+	return (chdir(program_path));
 }
-
+/**
+ * exit_shell - function to exit the shell
+ * @status: exit status
+ * Return: exit status
+ */
 int exit_shell(int status)
 {
-    exit(status);
+	exit(status);
 }
