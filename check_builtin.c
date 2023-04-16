b517ac35e3827593383e8e6d5f10183bb9a3f866
Dzrekey Daniel Kwasi <dkdzrekey001@gmail.com>
2023-04-16 22:33:23 +0000

    inital commit of check_builtin.c

--- check_builtin.c
+++ check_builtin.c
@@ -1,18 +1,22 @@
 #include "shell.h"
+/**
+ * check_builtins - check if command is a builtin
+ * @commands: array of commands args
+ * Return: 1 on success, 0 if unsuccessful
+ */
 
 int check_builtins(char **commands)
 {
-    if (strcmp(commands[0], "cd") == 0)
-    {
-        if (cd(commands[1]) < 0)
-            perror(commands[1]);
-            return(1);  
-    }
-    if (strcmp(commands[0], "exit") == 0)
-    {
-        exit_shell(EXIT_SUCCESS);
-        
-    }
+	if (strcmp(commands[0], "cd") == 0)
+	{
+		if (cd(commands[1]) < 0)
+			perror(commands[1]);
 
-    return (0);
+		return (1);
+	}
+	if (strcmp(commands[0], "exit") == 0)
+	{
+		exit_shell(EXIT_SUCCESS);
+	}
+	return (0);
 }
