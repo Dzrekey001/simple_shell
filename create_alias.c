20e8d447d35b1be35b88f616fad14109ca851568
Dzrekey Daniel Kwasi <dkdzrekey001@gmail.com>
2023-04-26 01:17:40 +0000

    Initial commit

---
+++ create_alias.c
@@ -0,0 +1,29 @@
+#include "shell.h"
+/**
+ * add_node - to create a new alias node
+ * @head: head of the node
+ * @name: alias name
+ * @args: alias argument
+ * Return: Null on failure, or return head of new node
+ */
+
+a_node *add_node(a_node **head, char *name, char *args)
+{
+	a_node *new_node;
+	int name_len = 0;
+	int arg_len = 0;
+
+	new_node = malloc(sizeof(a_node));
+
+	if (new_node == NULL)
+		return (NULL);
+	while (name[name_len])
+		name_len++;
+	while (args[name_len])
+		arg_len++;
+	new_node->alias_name = _strdup(name);
+	new_node->alias_args = _strdup(args);
+	new_node->next = *head;
+	*head = new_node;
+	return (new_node);
+}
---
+++ print_alias.c
@@ -0,0 +1,19 @@
+#include "shell.h"
+/**
+ * print_alias - function to print alais if it exist
+ * @alias: alias name
+ * Return: Always Nothing
+ */
+
+void print_alias(alias *alias)
+{
+	size_t i;
+
+	for (i = 0; alias[i].alias_name != NULL; i++)
+	{
+		write(STDOUT_FILENO, alias[i].alias_name, _strlen(alias[i].alias_name));
+		write(STDOUT_FILENO, "='", 3);
+		write(STDOUT_FILENO, alias[i].alias_args, _strlen(alias[i].alias_args));
+		write(STDOUT_FILENO, "'\n", 2);
+	}
+}
